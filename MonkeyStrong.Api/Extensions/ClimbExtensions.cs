e5b47367e50857513e01210bf2361236074e697a
MikeBeastall <github@mbcode.co.uk>
2015-08-24 14:54:47 +0100

    Missing file

---
+++ MonkeyStrong.Api/Extensions/ClimbExtensions.cs
@@ -0,0 +1,24 @@
+﻿using MonkeyStrong.Api.Models;
+using MonkeyStrong.Common.Responses;
+using MonkeyStrong.Common.TransferObjects;
+
+namespace MonkeyStrong.Api.Extensions
+{
+    public static class ClimbExtensions
+    {
+        public static ClimbResponse ToResponse(this Climb climb)
+        {
+            return new ClimbResponse
+            {
+                LatLong = new LatLongTransferObject
+                {
+                    Latitude = climb.LatLong.Latitude,
+                    Longitude = climb.LatLong.Longitude
+                },
+                Rating = climb.Rating,
+                Styles = climb.Styles,
+                Name = climb.Name
+            };
+        }
+    }
+}
